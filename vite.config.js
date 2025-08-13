ed8277a77f3babc72a9f6de76d0f5d51d7ad3fb7
Atul Yadav <150727962+AtulYadavCodes@users.noreply.github.com>
2025-08-13 13:48:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: './', // IMPORTANT: relative paths for Chrome
+  build: {
+    outDir: 'dist', // or "build" if you prefer
+    emptyOutDir: true
+  }
 })
